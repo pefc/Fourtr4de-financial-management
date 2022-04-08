a631466b07c92420048b5f0680b2fa0f9d4c4f41
Paulo Estevão Fragoso <paulo.estevao.fc@gmail.com>
2022-04-08 14:38:45 -0300

    ajustes

--- index.php
+++ index.php
@@ -21,15 +21,15 @@
 
 </head>
 
-<body class="bg-gray-100 font-sans leading-normal tracking-normal">
+<body class="bg-gray-800 font-sans leading-normal tracking-normal">
 
-    <nav id="header" class="bg-white fixed w-full z-10 top-0 shadow">
+    <nav id="header" class="bg-gray-700 fixed w-full z-10 top-0 shadow">
 
 
         <div class="w-full container mx-auto flex flex-wrap items-center mt-0 pt-3 pb-3 md:pb-0">
 
             <div class="w-1/2 pl-2 md:pl-2">
-                <a class="text-gray-900 text-base xl:text-xl no-underline hover:no-underline font-bold" href="#">
+                <a class="text-gray-100 text-base xl:text-xl no-underline hover:no-underline font-bold" href="#">
                     Gestão de banca
                 </a>
             </div>
@@ -38,12 +38,8 @@
 
                     <div class="relative text-sm">
                         <button id="userButton" class="flex items-center focus:outline-none mr-3">
-                            <img class="w-8 h-8 rounded-full mr-4" src="http://i.pravatar.cc/300" alt="Avatar of User"> <span class="hidden md:inline-block">Olá, Nome do Usuário</span>
-                            <svg class="pl-2 h-2" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 129 129" xmlns:xlink="http://www.w3.org/1999/xlink" enable-background="new 0 0 129 129">
-                                <g>
-                                    <path d="m121.3,34.6c-1.6-1.6-4.2-1.6-5.8,0l-51,51.1-51.1-51.1c-1.6-1.6-4.2-1.6-5.8,0-1.6,1.6-1.6,4.2 0,5.8l53.9,53.9c0.8,0.8 1.8,1.2 2.9,1.2 1,0 2.1-0.4 2.9-1.2l53.9-53.9c1.7-1.6 1.7-4.2 0.1-5.8z" />
-                                </g>
-                            </svg>
+                            <img class="w-8 h-8 rounded-full mr-4" src="http://i.pravatar.cc/300" alt="Avatar of User"> <span class="hidden md:inline-block text-gray-100">Olá, Nome do Usuário</span>
+                            <span class="pl-2 text-gray-100"><i class="fas fa-caret-down"></i></span>
                         </button>
                         <div id="userMenu" class="bg-white rounded shadow-md mt-2 absolute mt-12 top-0 right-0 min-w-full overflow-auto z-30 invisible">
                             <ul class="list-reset">
@@ -58,7 +54,7 @@
 
 
                     <div class="block lg:hidden pr-4">
-                        <button id="nav-toggle" class="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-gray-900 hover:border-teal-500 appearance-none focus:outline-none">
+                        <button id="nav-toggle" class="bg-gray-100 flex items-center px-3 py-2 border rounded text-gray-500 border-gray-500 hover:text-gray-500 hover:border-gray-500 appearance-none focus:outline-none">
                             <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                 <title>Menu</title>
                                 <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
@@ -70,7 +66,7 @@
             </div>
 
 
-            <div class="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden lg:block mt-2 lg:mt-0 bg-white z-20" id="nav-content">
+            <div class="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden lg:block mt-2 lg:mt-0 bg-gray-700 z-20" id="nav-content">
                 <ul class="list-reset lg:flex flex-1 items-center px-4 md:px-1">
                     <!-- <li class="mr-6 my-2 md:my-0">
                         <a href="index.html" class="block py-1 md:py-3 pl-1 align-middle text-green-600 no-underline hover:text-gray-900 border-b-2 border-orange-600 hover:border-orange-600">
@@ -88,22 +84,22 @@
                         </a>
                     </li> -->
                     <li class="mr-6 my-2 md:my-0" data-modal-toggle="reports-modal">
-                        <a href="#" class="block py-1 md:py-3 pl-1 align-middle text-gray-500 no-underline hover:text-gray-900 border-b-2 border-white hover:border-green-500">
+                        <a href="#" class="block py-1 md:py-3 pl-1 align-middle text-gray-100 no-underline hover:text-gray-500">
                             <i class="fas fa-chart-area fa-fw mr-3"></i><span class="pb-1 md:pb-0 text-sm">Relatórios</span>
                         </a>
                     </li>
                     <li class="mr-6 my-2 md:my-0" data-modal-toggle="withdrawal-modal">
-                        <a href="#" class="block py-1 md:py-3 pl-1 align-middle text-gray-500 no-underline hover:text-gray-900 border-b-2 border-white hover:border-green-500">
+                        <a href="#" class="block py-1 md:py-3 pl-1 align-middle text-gray-100 no-underline hover:text-gray-500">
                             <i class="fa fa-money-bill-transfer fa-fw mr-3"></i><span class="pb-1 md:pb-0 text-sm">Informar Saque</span>
                         </a>
                     </li>
                     <li class="mr-6 my-2 md:my-0" data-modal-toggle="operation-modal">
-                        <a href="#" class="block py-1 md:py-3 pl-1 align-middle text-gray-500 no-underline hover:text-gray-900 border-b-2 border-white hover:border-green-500">
+                        <a href="#" class="block py-1 md:py-3 pl-1 align-middle text-gray-100 no-underline hover:text-gray-500">
                             <i class="fa fa-tasks fa-fw mr-3"></i><span class="pb-1 md:pb-0 text-sm" >Informar Operação</span>
                         </a>
                     </li>
                     <li class="mr-6 my-2 md:my-0" data-modal-toggle="config-modal">
-                        <a href="#" class="block py-1 md:py-3 pl-1 align-middle text-gray-500 no-underline hover:text-gray-900 border-b-2 border-white hover:border-green-500">
+                        <a href="#" class="block py-1 md:py-3 pl-1 align-middle text-gray-100 no-underline hover:text-gray-500">
                             <i class="fa fa-cog fa-fw mr-3"></i><span class="pb-1 md:pb-0 text-sm">Configurações</span>
                         </a>
                     </li>
@@ -138,7 +134,7 @@
             <div class="flex flex-wrap">
                 <div class="w-full md:w-1/2 xl:w-1/3 p-3">
                     <!--Metric Card-->
-                    <div class="bg-white border rounded shadow p-2">
+                    <div class="bg-gray-200 border rounded shadow p-2">
                         <div class="flex flex-row items-center">
                             <div class="flex-shrink pr-4">
                                 <div class="rounded p-3 bg-green-600"><i class="fa fa-wallet fa-2x fa-fw fa-inverse"></i></div>
@@ -154,7 +150,7 @@
 
                 <div class="w-full md:w-1/2 xl:w-1/3 p-3">
                     <!--Metric Card-->
-                    <div class="bg-white border rounded shadow p-2">
+                    <div class="bg-gray-200 border rounded shadow p-2">
                         <div class="flex flex-row items-center">
                             <div class="flex-shrink pr-4">
                                 <div class="rounded p-3 bg-green-600"><i class="fa fa-wallet fa-2x fa-fw fa-inverse"></i></div>
@@ -170,7 +166,7 @@
 
                 <div class="w-full md:w-1/2 xl:w-1/3 p-3">
                     <!--Metric Card-->
-                    <div class="bg-white border rounded shadow p-2">
+                    <div class="bg-gray-200 border rounded shadow p-2">
                         <div class="flex flex-row items-center">
                             <div class="flex-shrink pr-4">
                                 <div class="rounded p-3 bg-green-600"><i class="fas fa-percent fa-2x fa-fw fa-inverse"></i></div>
@@ -186,7 +182,7 @@
 
                 <div class="w-full md:w-1/2 xl:w-1/3 p-3">
                     <!--Metric Card-->
-                    <div class="bg-white border rounded shadow p-2">
+                    <div class="bg-gray-200 border rounded shadow p-2">
                         <div class="flex flex-row items-center">
                             <div class="flex-shrink pr-4">
                                 <div class="rounded p-3 bg-blue-600"><i class="fas fa-tasks fa-2x fa-fw fa-inverse"></i></div>
@@ -204,7 +200,7 @@
 
                 <div class="w-full md:w-1/2 xl:w-1/3 p-3">
                     <!--Metric Card-->
-                    <div class="bg-white border rounded shadow p-2">
+                    <div class="bg-gray-200 border rounded shadow p-2">
                         <div class="flex flex-row items-center">
                             <div class="flex-shrink pr-4">
                                 <div class="rounded p-3 bg-blue-600"><i class="fas fa-file-invoice-dollar fa-2x fa-fw fa-inverse"></i></div>
@@ -220,7 +216,7 @@
                 
                 <div class="w-full md:w-1/2 xl:w-1/3 p-3">
                     <!--Metric Card-->
-                    <div class="bg-white border rounded shadow p-2">
+                    <div class="bg-gray-200 border rounded shadow p-2">
                         <div class="flex flex-row items-center">
                             <div class="flex-shrink pr-4">
                                 <div class="rounded p-3 bg-blue-600"><i class="fas fa-sack-dollar fa-2x fa-fw fa-inverse"></i></div>
@@ -237,22 +233,22 @@
             </div>
 
             <!--Divider-->
-            <hr class="border-b-2 border-gray-400 my-8 mx-4">
+            <hr class="border-b-5 border-gray-100 my-8 mx-4">
 
             <div class="flex flex-row flex-wrap flex-grow mt-2">
 
-                <div class="w-full   p-3">
+                <div class="w-full p-3">
                     <!--Template Card-->
                     <div class="bg-white border rounded shadow">
-                        <div class="border-b p-3">
-                            <h5 class="font-bold uppercase text-gray-600">Valor para entradas</h5>
+                        <div class="bg-gray-200 border-b p-3">
+                            <h5 class="font-bold uppercase text-gray-600">Gestão da banca</h5>
                         </div>
                         <div class="p-5">
-                            <table class="w-full p-5 text-gray-700">
+                            <table class="w-full p-5 text-gray-700 text-center">
                                 <thead>
-                                    <tr>
-                                        <th class="text-left text-blue-900">Porcentagem</th>
-                                        <th class="text-left text-blue-900">Valor</th>
+                                    <tr class="bg-gray-200 text-blue-900">
+                                        <th>Porcentagem</th>
+                                        <th>Valor da aposta</th>
                                     </tr>
                                 </thead>
 
@@ -261,7 +257,7 @@
                                         <td>0,5%</td>
                                         <td>R$ 5,00</td>
                                     </tr>
-                                    <tr>
+                                    <tr class="bg-gray-100">
                                         <td>1%</td>
                                         <td>R$ 10,00</td>
                                     </tr>
@@ -269,7 +265,7 @@
                                         <td>1,5%</td>
                                         <td>R$ 20,00</td>
                                     </tr>
-                                    <tr>
+                                    <tr class="bg-gray-100">
                                         <td>2%</td>
                                         <td>R$ 80,00</td>
                                     </tr>
@@ -277,7 +273,7 @@
                                         <td>3,5%</td>
                                         <td>R$ 80,00</td>
                                     </tr>
-                                    <tr>
+                                    <tr class="bg-gray-100">
                                         <td>4%</td>
                                         <td>R$ 80,00</td>
                                     </tr>
@@ -285,7 +281,7 @@
                                         <td>7%</td>
                                         <td>R$ 80,00</td>
                                     </tr>
-                                    <tr>
+                                    <tr class="bg-gray-100">
                                         <td>8%</td>
                                         <td>R$ 80,00</td>
                                     </tr>
@@ -471,6 +467,7 @@
                 </div>
             </div>
         </div>
+        
 
         <div id="reports-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full justify-center items-center">
             <div class="relative p-4 w-full max-w-md h-full md:h-auto">
@@ -579,14 +576,14 @@
     </div>
     <!--/container-->
 
-    <footer class="bg-white border-t border-gray-400 shadow">
+    <footer class="bg-gray-700 border-t border-gray-400 shadow">
         <div class="container max-w-md mx-auto flex py-8">
 
             <div class="w-full mx-auto flex flex-wrap">
                 <div class="flex w-full md:w-1/2 ">
                     <div class="px-8">
-                        <h3 class="font-bold font-bold text-gray-900">Quem somos</h3>
-                        <p class="py-4 text-gray-600 text-sm">
+                        <h3 class="font-bold font-bold text-gray-100">Quem somos</h3>
+                        <p class="py-4 text-gray-400 text-sm">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vel mi ut felis tempus commodo nec id erat. Suspendisse consectetur dapibus velit ut lacinia.
                         </p>
                     </div>
@@ -594,16 +591,16 @@
 
                 <div class="flex w-full md:w-1/2">
                     <div class="px-8">
-                        <h3 class="font-bold font-bold text-gray-900">Contato</h3>
+                        <h3 class="font-bold font-bold text-gray-100">Contato</h3>
                         <ul class="list-reset items-center text-sm pt-3">
                             <li>
-                                <i class="fa-brands fa-instagram"></i> <a class="inline-block text-gray-600 no-underline hover:text-gray-900 hover:underline py-1" href="https://www.instagram.com/fourtr4de/">@fourtr4de</a>
+                                <span class="text-gray-100"><i class="fa-brands fa-instagram"></i></span> <a class="inline-block text-gray-400 no-underline hover:text-gray-100 hover:no-underline py-1" href="https://www.instagram.com/fourtr4de/">Intagram</a>
                             </li>
                             <li>
-                                <i class="fa-brands fa-telegram"></i> <a class="inline-block text-gray-600 no-underline hover:text-gray-900 hover:underline py-1" href="#">Telagram</a>
+                                <span class="text-gray-100"><i class="fa-brands fa-telegram"></i></span> <a class="inline-block text-gray-400 no-underline hover:text-gray-100 hover:no-underline py-1" href="#">Telagram</a>
                             </li>
                             <li>
-                                <i class="fa-brands fa-whatsapp"></i> <a class="inline-block text-gray-600 no-underline hover:text-gray-900 hover:underline py-1" href="#">Whatsapp</a>
+                                <span class="text-gray-100"><i class="fa-brands fa-whatsapp"></i></span> <a class="inline-block text-gray-400 no-underline hover:text-gray-100 hover:no-underline py-1" href="#">Whatsapp</a>
                             </li>
                         </ul>
                     </div>

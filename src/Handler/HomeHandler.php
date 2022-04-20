e483e5c5e40f6b4a7177be7464f89aa38183f791
Paulo Estevão Fragoso <paulo.estevao.fc@gmail.com>
2022-04-20 00:35:05 -0300

    ajustes para o slim

--- src/Handler/HomeHandler.php
+++ src/Handler/HomeHandler.php
@@ -40,8 +40,86 @@ class HomeHandler implements RequestHandlerInterface
     public function handle(ServerRequestInterface $request): ResponseInterface
     {        
         $response = new Response();
+
+        $bankrollDayBefore = rand(30,10000);
+        $bankroll = rand(1,10000);
+        $percentBankrollYield = ((($bankroll*100)/$bankrollDayBefore)-100);
+        
+        $colorPercentBankrollYield = "yellow";
+        if ( $percentBankrollYield > 0)
+            $colorPercentBankrollYield = "green";
+        elseif ( $percentBankrollYield < 0)
+            $colorPercentBankrollYield = "red";
+
+
+        $dailyYieldBefore = rand(0,500);
+        $dailyYield = rand(0,500);
+        $colorDailyYield = "yellow";
+        $narrowDailyYield = "minus";
+        if ( $dailyYield > $dailyYieldBefore)
+        {
+            $colorDailyYield = "green";
+            $narrowDailyYield = "caret-up";
+        }
+        elseif ( $dailyYield < $dailyYieldBefore)
+        {
+            $colorDailyYield = "red";
+            $narrowDailyYield = "caret-down";
+        }
+
+
+        $accumulatedYieldBefore = rand(30,10000);
+        $accumulatedYield = rand(30,10000);
+        $colorAccumulatedYield = "yellow";
+        $narrowAccumulatedYield = "minus";
+        if ( $accumulatedYield > $accumulatedYieldBefore)
+        {
+            $colorAccumulatedYield = "green";
+            $narrowAccumulatedYield = "caret-up";
+        }
+        elseif ( $accumulatedYield < $accumulatedYieldBefore)
+        {
+            $colorAccumulatedYield = "red";
+            $narrowAccumulatedYield = "caret-down";
+        }
+
+        $profitMargin = rand(2,20);
+
+        $goal = ($bankroll*$profitMargin)/100;
+
+        $percenteGoal = ($dailyYield*100)/$goal;
+        if ($percenteGoal >= 100)
+            $percenteGoal = 100;
+
+        $stopWin = (($bankrollDayBefore*0.3)+$bankrollDayBefore);
+
+        $stopLoss = ($bankrollDayBefore-($bankrollDayBefore*0.2));
+
+        $args = [
+            'bankroll' => number_format($bankroll,2,",","."),
+
+            'percentBankrollYield' => number_format($percentBankrollYield,2,",","."),
+            'colorPercentBankrollYield' => $colorPercentBankrollYield,
+
+            'dailyYield' => number_format($dailyYield,2,",","."),
+            'colorDailyYield' => $colorDailyYield,
+            'narrowDailyYield' => $narrowDailyYield,
+
+            'accumulatedYield' => number_format($accumulatedYield,2,",","."),
+            'colorAccumulatedYield' => $colorAccumulatedYield,
+            'narrowAccumulatedYield' => $narrowAccumulatedYield,
+
+            'profitMargin' => $profitMargin,
+
+            'goal' => number_format($goal,2,",","."),
+            'percenteGoal' => $percenteGoal,
+            
+            'stopWin' => number_format($stopWin,2,",","."),
+            'stopLoss' => number_format($stopLoss,2,",","."),
+        ];
+
         $response->getBody()->write(
-            $this->twig->render('home.twig')
+            $this->twig->render('home.twig', $args)
         );
         return $response;
     }
